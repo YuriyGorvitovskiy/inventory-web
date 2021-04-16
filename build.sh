612ade2ddd715b09c0b6688a8a402c296f1934a2
Yuriy Gorvitovskiy <yuriy@centricsoftware.com>
2021-04-15 20:36:43 -0700

    adding k8s configuration

--- build.sh
+++ build.sh
@@ -5,4 +5,9 @@ gcp -ur public/* build/
 
 docker build -f Dockerfile -t yuriyg/inventory-web:0.0.1 ./build/
 
+# run localy
 # docker run -d -p 3780:80 --name inventory-web yuriyg/inventory-web:0.0.1
+
+# publish to Docker hub
+# docker login --username=yuriyg 
+# docker push yuriyg/inventory-web:0.0.1
