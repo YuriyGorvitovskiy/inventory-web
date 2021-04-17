c301ecfb1461e41c8b850ec3143592b964b8de7e
Yuriy Gorvitovskiy <yuriy@centricsoftware.com>
2021-04-16 19:30:06 -0700

    Update local docker port

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ gcp -ur public/* build/
 docker build -f Dockerfile -t yuriyg/inventory-web:0.0.1 ./build/
 
 # run localy
-# docker run -d -p 3780:80 --name inventory-web yuriyg/inventory-web:0.0.1
+# docker run -d -p 3701:80 --name inventory-web yuriyg/inventory-web:0.0.1
 
 # publish to Docker hub
 # docker login --username=yuriyg 
